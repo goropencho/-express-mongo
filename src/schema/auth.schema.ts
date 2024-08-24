667de3e8adaeacf53c8abde1fdbdc05e71834ca2
Gaurav Panchal <gkp.125@gmail.com>
2024-08-25 01:19:30 +0530

    Schema Updated

--- src/schema/auth.schema.ts
+++ src/schema/auth.schema.ts
@@ -8,7 +8,10 @@ export const SignUpSchema = z.object({
 
 export type SignUpInterface = z.infer<typeof SignUpSchema>;
 
-export const SignInSchema = SignUpSchema.extend({});
+export const SignInSchema = z.object({
+  email: z.string().email({message: 'Invalid Email'}),
+  password: z.string().min(8, {message: 'Required Minimum Length 8'}),
+});
 
 export type SignInInterface = z.infer<typeof SignInSchema>;
 
