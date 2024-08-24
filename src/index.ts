46985ef60ef16ab8ed40432c274b1f8237b5a0d9
Gaurav Panchal <gkp.125@gmail.com>
2024-08-24 14:44:00 +0530

    Mongoose Types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express = require('express');
 import cors = require('cors');
 import {env} from './config/config';
 import {errorHandler} from './utils/errorHandler';
+import mongoose from 'mongoose';
 const app = express();
+
+mongoose.connect(env.MONGODB_URL).then(() => {
+  console.log('Connected to Database');
+});
 
 app.use(cors());
 app.use(express.json());
-
 app.get('/', (req, res) => {
   return res.send({...req.body, id: 1, ...env});
 });
