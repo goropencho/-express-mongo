571f287f549eba5834d4293bc29ca080f1d954fe
Gaurav Panchal <gkp.125@gmail.com>
2024-08-24 14:39:06 +0530

    Error Handler

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,5 +1,6 @@
 import {NextFunction, Request, Response} from 'express';
 import {BadRequestException, InternalServerError} from './exceptions';
+import {HttpException} from './exceptions/http.exception';
 
 export const errorHandler = (
   err: Error,
@@ -7,10 +8,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof BadRequestException) {
-    res.status(400).json({message: err.message});
-  } else if (err instanceof InternalServerError) {
-    res.status(500).json({message: 'Internal Server Error'});
+  if (err instanceof HttpException) {
+    res.status(err.statusCode).json({message: err.message});
   } else {
     res.status(500).json({message: 'Unexpected Error'});
   }
